d16b2a1138e1dbb55864a76db1b622cd5de64908
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2024-01-26 02:13:29 +0800

    added formatting lines

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ from azure_verify import get_user_id
 from azure_delete import delete_user
 
 target_email = os.environ['Email']
-tenant_id = "abf9f4cd-56cd-4d8a-9a02-7c288b91d9a5"
+
+env_file = open("temp_env.txt")
+target_environment = env_file.read()
+target_environment = target_environment.replace("\n","")
+
+tenant_id_lookup = {
+	"Deltekcloud":"abf9f4cd-56cd-4d8a-9a02-7c288b91d9a5",
+	"Conceptshare":"05142a66-17ef-42ce-9c1e-9690d507700d",
+	"ITConceptshare":"44ecf5c5-5945-4f24-96ec-a73db8335538"
+}
+
+tenant_id = tenant_id_lookup[target_environment]
 
 if '@' not in target_email or 'deltek.com' not in target_email:
-	print("\n--------------\nERROR! Unexpected or malformed email format! Format should be <user>@deltek.com\n--------------\n")
+	print("\n------------------------------------------\nERROR! Unexpected or malformed email format! Format should be <user>@deltek.com\n------------------------------------------\n")
 	exit(0)
 
 
