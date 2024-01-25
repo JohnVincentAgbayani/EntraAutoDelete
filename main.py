641717858f97536398edfaf464360cb41582a278
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2024-01-26 01:48:49 +0800

    input validation for email format

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+
 from azure_verify import get_user_id
 from azure_delete import delete_user
 
-target_email = "disabletest3@deltek.com"
+target_email = os.environ['Email']
 tenant_id = "abf9f4cd-56cd-4d8a-9a02-7c288b91d9a5"
+
+if '@' not in target_email or 'deltek.com' not in target_email:
+	print("--------------\nERROR! Unexpected or malformed email format! Format should be <user>@deltek.com\n--------------\n")
+
 
 target_id = get_user_id(target_email, tenant_id)
 if "match" not in target_id:
