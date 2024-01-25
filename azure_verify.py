083e6a6a95a39ca1c99993f27a2d74b80aef61c5
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2024-01-26 03:36:49 +0800

    removed troubleshooting markers

--- azure_verify.py
+++ azure_verify.py
@@ -11,7 +11,6 @@ def get_user_id(target_email, tenant_id, api_id, api_secret):
 
 	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 	response = requests.post(f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token", headers=headers, data=data)
-	print(response)
 	bearer_token = response.json()['access_token']
 
 	cont = True
